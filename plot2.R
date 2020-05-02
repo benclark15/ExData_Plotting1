a9aec84b2060650edd47bed2431bec5b8aa6a18c
Ben Clark <benclark15@hotmail.com>
2020-05-02 10:36:14 -0400

    Committing plot scripts and images.

---
+++ plot2.R
@@ -0,0 +1,44 @@
+library(data.table)
+library(dplyr)
+library(lubridate)
+
+Sys.setenv(LANG = "en")
+
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+## Only download if not yet downloaded
+if (!file.exists("household_power_consumption.zip")){
+  print("Downloading...")
+  download.file(fileURL, destfile = "household_power_consumption.zip")  
+  dateDownloaded = date()
+  print(dateDownloaded)
+}
+
+## Only unzip if not yet unzipped
+if (!file.exists("household_power_consumption.txt")){
+  print("Unzipping...")
+  unzip("household_power_consumption.zip")
+}
+
+## Read data, extracting the data for the approximate date range required
+energy_ds_all <- read.table("household_power_consumption.txt", skip = 66000, nrows = 4000, na.strings = "?", sep = ";")
+
+## Change the column names to something more descriptive
+new_col_names <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+colnames(energy_ds_all) <- new_col_names
+
+## Now just get the data for the dates 1st and 2nd Feb 2007
+energy_ds <- subset(energy_ds_all, Date == "1/2/2007" | Date == "2/2/2007")
+
+## Reset the Date column to a manageable format
+energy_ds <- mutate(energy_ds, Date = dmy(Date))
+
+## Create a new column with date and time in same variable
+energy_ds <- mutate(energy_ds, Date_Time = ymd_hms(paste(Date,Time)))
+
+## Print Plot 2 - Please note I can only put day names in Spanish
+## as I am in Chile and my OS does not allow me to change locale
+png("plot2.png", width = 480, height = 480)
+plot(energy_ds$Date_Time, energy_ds$Global_active_power,type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.off()
